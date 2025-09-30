765c2f645e964bbf1642726b984e36c174958f89
howdopointerswork <dominusfaba@outlook.com>
2025-09-29 21:28:06 -0700

    Initial commit. Front-end WIP. Back-end coming soon

---
+++ survey.js
@@ -0,0 +1,23 @@
+var script = document.createElement('script');
+
+
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+script.type = "text/javascript";
+
+
+
+script.onload = function(){
+	
+	let auth, signup = false;
+	let signout = true;
+
+	$(document).ready(function(){
+
+
+	});
+
+
+};
+
+
+document.head.appendChild(script);
