d675a9f36ce7c2bf7cf2cc757e4ef629510152ae
howdopointerswork <dominusfaba@outlook.com>
2025-10-14 22:03:17 -0700

    Beta almost ready

--- survey.js
+++ survey.js
@@ -4,14 +4,67 @@ var script = document.createElement('script');
 script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
 script.type = "text/javascript";
 
-
+let page = 0;
 
 script.onload = function(){
 	
-
+	console.log("loaded");
 	$(document).ready(function(){
 
-		console.log("Hello");
+
+
+		$('#next').click(function(){
+
+			++page;
+			console.log(page);
+			$('html, body').animate({ scrollTop: 0 }, 'slow');
+			$('#current').html((page+1).toString());
+
+			if(page > 0){
+
+				$('#prev').css('visibility', 'visible');
+				$('#header').html('Student Interest Survey: Clubs & Groups');
+			}
+			
+			
+			if(page == 16){
+
+				//$('#prev').css('visibility', 'hidden');
+				$('#next').css('visibility', 'hidden');
+				$('#submit').css('display', '');
+
+			}
+
+			$('#'+(page-1).toString()).css('display', 'none');
+			$('#'+page.toString()).css('display', '');
+
+			//account for last page, make next invisible
+			//and show submit
+
+		});
+
+		$('#prev').click(function(){
+
+			--page;
+			console.log(page);
+			$('html, body').animate({ scrollTop: 0 }, 'slow');
+			$('#current').html((page+1).toString());
+
+			if(page == 0){
+
+				$('#prev').css('visibility', 'hidden');
+				$('#header').html('Petition To Expand Shuttle Bus Service to KPU Richmond Campus');
+
+			}
+
+			$('#'+(page+1).toString()).css('display', 'none');
+			$('#'+page.toString()).css('display', '');
+
+		});
+
+		
+
+
 	});
 
 
--- survey.php
+++ survey.php
@@ -3,6 +3,22 @@
 //function that creates var and inserts into array
 //create array of label names and pass as well outside function
 
+/*to-do:
+- Input verification, no empties
+- Concat function
+- Posts should be filtered
+
+
+
+
+*/
+
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);   
+
+
+
 function processResult($name, $inputs){
 
 	$result = $_POST[$name] ?? '';
@@ -11,29 +27,28 @@ function processResult($name, $inputs){
 
 }
 
+
+	
+
+
+
 	$names = ['a1', 'a2', 'b1', 'b2', 'b3', 'c', 'd1', 'd2', 'e1', 'e2', 'f1', 'f2', 'g1', 'g2', 'g3', 'g4', 'h1', 'h2', 'h3', 'h4', 'i1', 'i2', 'j1', 'j2', 'j3', 'k1', 'k2', 'l1', 'l2', 'l3', 'l4', 'l5', 'con', 'opt'];
+
+	$ids = [4 => "Check", 5 => "Check", 9 => "String", 10 => "String", 11 => "String", 12 => "Scale", 13 => "Scale", 14 => "Scale", 15 => "Scale", 23 => "String", 24 => "String", 25 => "String", 26 => "String", 27 => "String", 28 => "String", 29 => "String", 30 => "String"];
+
+
+	$i = 0;
 
 	if($_SERVER["REQUEST_METHOD"] === "POST"){
 
-		processResult($names[0], $names);
-		processResult($names[1], $names);
-		processResult($names[2], $names);
 
+		//processResult($names[0], $names);
 
-/*		$result_b1 = $_POST['b1'] ?? '';
-
-		echo $result_b1;*/
-
-		/*$result_b2 = $_POST['b2'] ?? '';
-
-		echo $result_b2;*/
+		$action = $_POST['action'] ?? '';
 
 
 
-		
-
-
-	$db = new SQLite3("survey.db");
+		$db = new SQLite3("survey.db");
 
 
 	$db->exec("CREATE TABLE IF NOT EXISTS results (
@@ -90,6 +105,108 @@ function processResult($name, $inputs){
 )");
 
 
+	$db->exec("CREATE TABLE IF NOT EXISTS petition (
+
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+		name TEXT NOT NULL,
+		email TEXT NOT NULL,
+		signature TEXT NOT NULL
+
+
+)");
+
+
+		//add each to array for insertion
+		//sign entry separately
+	if($action == "Submit"){
+
+		echo "Post<br>";
+
+		foreach($names as $index => $name){
+
+			//echo "<br>index: " . $index;
+			//echo "<br>name: " . $name;
+			echo "<br>index: " . $index . "<br>";
+			echo "i: " . $i . "<br>";
+			echo "name: " . $name . "<br>";
+			
+				
+			if($index==$i++ && isset($ids[$i])){
+
+				echo "Set and match<br>";
+
+				if(!empty($_POST[$name])){
+
+					$arr = $_POST[$name];
+
+					echo "good<br>";
+
+					switch(strtolower($ids[$i])){
+
+						case "check":
+							//loop arr and concat
+							echo "Concat<br>";
+							break;
+
+						case "string":
+							//loop arr and encode
+							echo "String<br>";
+							break;
+
+						case "scale":
+							//loop arr and measure scale using step?
+							echo "Scale<br>";
+							break;
+
+						default:
+						 	echo "Radio<br>";
+
+
+					}
+					
+					
+
+					//switch
+
+					//exception names array with i var for index
+			
+					
+
+					
+
+			}
+				
+			}else{
+				
+				echo $_POST[$name] . "<br>";
+			}
+	
+	}
+
+}
+	elseif($action == "sign"){
+
+		echo "sign";
+	}
+		
+
+/*		$result_b1 = $_POST['b1'] ?? '';
+
+		echo $result_b1;*/
+
+		/*$result_b2 = $_POST['b2'] ?? '';
+
+		echo $result_b2;*/
+
+
+
+		
+
+
+
+
+
 
 	//test values 
 
@@ -98,6 +215,8 @@ function processResult($name, $inputs){
 	$db->close();
 
 }
+
+
 
 
 ?>
